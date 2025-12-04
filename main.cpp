a4e8206469f19d88d155bff10b1bee48d49381b1
DigitalMelquiades <shavl1gaming06@gmail.com>
2025-12-04 15:18:02 +0400

    Add details

--- main.cpp
+++ main.cpp
@@ -9,11 +9,10 @@ class Deque {
         Node* next;
         Node* prev;
         Node(const int& data) : data(data), next(nullptr), prev(nullptr) {}
-        void display() const { std::cout<< data; }
     };
     Node* head;
     Node* tail;
-    std::vector<Node> servedOrders;
+    std::vector<int> servedOrders;
 public:
     Deque() : head(nullptr), tail(nullptr) {}
     Deque(const Deque& other) {
@@ -37,13 +36,7 @@ public:
     }
     bool isEmpty() const { return head == nullptr; }
     bool isRecurring(const int& value) const {
-        /*Node* it = head;
-        while (it != nullptr) {
-            if (it->data == value) return true;
-            it = it->next;
-        }
-        return false;*/
-        for (Node order:servedOrders) if (order.data == value) return true;
+        for (const int& order:servedOrders) if (order == value) return true;
         return false;
     }
     void enque(const int& value) {
@@ -65,7 +58,7 @@ public:
     void deque() {
         if (isEmpty()) throw std::out_of_range("The list is empty!\n");
         Node* temp = head;
-        servedOrders.push_back(*temp);
+        if (!isRecurring(temp->data)) servedOrders.push_back(temp->data); // To avoid repeating server orders
         if (head == tail) head=tail=nullptr; // Case if we have only one element
         else {
             head = head->next;
@@ -81,17 +74,20 @@ public:
         }
     }
     void seePopped() {
-        if (!servedOrders.empty()) {
-            for (Node& pop:servedOrders) {
-                pop.display();
-                std::cout<<' ';
-            }
-        }
+        if (!servedOrders.empty()) for (const int& order:servedOrders) std::cout<<order<<' ';
         else std::cout<<"There is no popped element(s)!";
     }
     Deque& operator=(const Deque& other) {
         if (this != &other) { // To check self assignment
-            this->~Deque();
+            this->~Deque(); // Even though we used this approach on tha Lab, after research it seems to be a cause of corrupt memory, hence second approach:
+            /*Node* cur = head;
+            while (cur) {
+                Node* next = cur->next;
+                delete cur;
+                cur = next;
+            }
+            head = tail = nullptr;*/
+            servedOrders.clear();
             if (!other.isEmpty()) {
                 this->head=this->tail = new Node(other.head->data);
                 Node* otherit= other.head->next;
@@ -113,7 +109,7 @@ std::ostream& operator<<(std::ostream& out, Deque& other) {
 }
 
 int main() {
-    class Deque deq;
+    Deque deq;
     deq.enque(1);
     deq.enque(2);
     deq.enque(3);
@@ -121,11 +117,11 @@ int main() {
     deq.enque(5);
     deq.enque(6);
 
-    std::cout<<"===Deque 1===\n"<<deq<<std::endl;
-    deq.deque();
-    deq.deque();
-    deq.enque(1);
-    deq.enque(2);
+    std::cout<<"===Deque 1===\n"<<deq<<std::endl; // { 1, 2, 3, 4, 5, 6 }
+    deq.deque(); // { 2, 3, 4, 5, 6 } added 1 to the servedOrders -> { 1 }
+    deq.deque(); // { 3, 4, 5, 6 } added 2 to the servedOrders -> { 1, 2 }
+    deq.enque(1); // { 1, 3, 4, 5, 6 }
+    deq.enque(2); // { 2, 1, 3, 4, 5, 6 }
 
     std::cout<<deq<<"\nSee popped elements: ";
     deq.seePopped();
@@ -135,12 +131,12 @@ int main() {
     std::cout<<std::endl<<deq<<"\nSee popped elements: ";
     deq.seePopped();
 
-    class Deque deq2 = deq; // Assignment operator
-    class Deque deq3(deq2); // Copy constructor
+    Deque deq2 = deq; // Assignment operator
+    Deque deq3(deq2); // Copy constructor
 
-    std::cout<<"\n===Deque 2===\nAssignment operator wokring: "<<deq2<<"\nPopped elements: ";
+    std::cout<<"\n===Deque 2===\nDeque: "<<deq2<<" // Assignment operator working\nPopped elements: ";
     deq2.seePopped(); // Also validates if there is popped elements or not, hence no bugs, nor logical errors occur
-    std::cout<<"\n===Deque 3===\n";
+    std::cout<<"\n===Deque 3===\nDeque: "<<deq3<<" // Copy constructor working\n";
     std::cout<<"Before enque: "<<deq3<<std::endl;
     deq3.enque(7);
     std::cout<<"After enque: "<<deq3<<std::endl;
